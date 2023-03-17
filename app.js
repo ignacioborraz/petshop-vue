238301f524cdfe2398225ab9172212d808c28933
ignacioborraz <ignacioborraz@hotmail.com>
2023-03-17 19:40:44 -0300

    app

--- app.js
+++ app.js
@@ -1,31 +1,62 @@
 const { createApp } = Vue
 const url = 'https://mindhub-xj03.onrender.com/api/petshop'
 
-const app = createApp( {
-    data(){
+const app = createApp({
+    data(){         /* funcion que define los estados (propiedades reactivas) de la aplicación */
         return {
-            name: 'PETSHOP MINDY',
+            /* clave: valor */
+            name: "MINDY",
+            description: "Mindy es una tienda de mascotas. Vendemos juguetes y remedios para tus animalitos.",
+            subtitle: "PRODUCTOS",
             products: [],
-            remedies: [],
+            categories: []
         }
     },
-    created() {
-        this.fetchApi()
+    created() {     /* hook con las funciones que se ejecutan luego de cargar las variables de estado */
+        /* ejecuto funcion() */
+        //this.fetchApi()
+        //this.getCategories()
+        this.fetchOnlyOne()
     },
-    methods: {
+    methods: {      /* objeto con todos los métodos que necesita la app (fetch/filter/etc) */
+        /* defino funcion */
         async fetchApi() {
             try {
                 let response = await fetch(url)
                 response = await response.json()
-                this.products = response.filter(each=>each.categoria==='jugueteria')
-                this.remedies = response.filter(each=>each.categoria==='farmacia')
-                console.log(this.products)
-                console.log(this.remedies)
+                //console.log(response)
+                this.products = response
+            } catch(error) {
+                console.log(error)
+            }
+        },
+        async getCategories() {
+            try {
+                let response = await fetch(url) //fetcheo la api
+                response = await response.json() //transformo la respuesta en los datos que necesito
+                response = response.map(each => each.categoria) //transformo el array de objetos en un array de categorias
+                response = [...new Set(response)] //transformo el set nuevamente en un array (porque las props/metodos del set son limitados)
+                //console.log(response)
+                this.categories = response
+                console.log(this.categories)
+            } catch(error) {
+                console.log(error)
+            }
+        },
+        async fetchOnlyOne() {
+            try {
+                let response = await fetch(url) //fetcheo la api
+                response = await response.json()
+                this.products = response
+                response = response.map(each => each.categoria)
+                response = [...new Set(response)]
+                this.categories = response
             } catch(error) {
                 console.log(error)
             }
         }
+
     }
 })
 
-app.mount("#app")
+app.mount("#app")   /* monto la app de VUE */
